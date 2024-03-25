8ccd6e595f5bea9040523fb5da44ccbde026ffa2
CRE-A <121703704+CRE-A@users.noreply.github.com>
2024-03-25 20:56:45 +0900

    C:/Program Files/Git/home/main 추가

--- Back/src/main/java/com/jnb/animaldoctor24/domain/member/api/UserController.java
+++ Back/src/main/java/com/jnb/animaldoctor24/domain/member/api/UserController.java
@@ -6,11 +6,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/home")
 @RequiredArgsConstructor
 public class UserController {
     private final AuthenticationService authenticationService;
+
+    @GetMapping("/main")
+    public ResponseEntity<ResponseEntity<String>> home () {
+        System.out.println("/home/main 입장!");
+        return null;
+    }
+
     @PostMapping("/register")
     public ResponseEntity<ResponseEntity<String>> signup(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.register(request));
