21a370e456ae37a96c6b5b31f53ade67256bb10e
홍혜빈 <hongbin9696@gmail.com>
2024-03-26 23:45:51 +0900

    css수정

--- Front/animaldoctor24/src/pages/LoginPage.jsx
+++ Front/animaldoctor24/src/pages/LoginPage.jsx
@@ -15,16 +15,14 @@ export default function LoginPage() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // Here you can perform your authentication logic
         console.log('Logging in with:', { username, password });
-        // For demonstration purposes, let's clear the form fields after submission
         setUsername('');
         setPassword('');
     };
     return (
         <>
             <section className="relative flex flex-col items-center bg-white w-[393px] h-[782px] ">
-                <button className="w-[126px] h-[49px] rounded-[27px] bg-yellow-300 font-bold text-white text-lg mt-[50px]">LOGIN</button>
+                <button className="w-[126px] h-[49px] rounded-[27px] bg-yellow-300 font-bold text-white text-[15px] mt-[50px]">로그인</button>
                 <h1 className="font-bold relative top-[15px] text-lg">Animal Doctor 24 로그인하기!</h1>
                 <div className="relative bottom-[30px]">
                     <img src="https://loosedrawing.com/assets/illustrations/png/593.png" className="w-[316px] h-[316px]"/>
@@ -32,7 +30,8 @@ export default function LoginPage() {
                 <form onSubmit={handleSubmit} className="relative bottom-[70px]">
                     <div>
                         <input
-                            className="w-[342px] h-[54px] border-2 mb-[20px]"
+                            className="w-[342px] h-[54px] border-2 mb-[20px] focus:outline-none px-[10px] "
+                            placeholder="이메일"
                             type="text"
                             id="username"
                             value={username}
@@ -42,7 +41,8 @@ export default function LoginPage() {
                     </div>
                     <div>
                         <input
-                            className="w-[342px] h-[54px] border-2 mb-[20px]"
+                            className="w-[342px] h-[54px] border-2 mb-[20px] focus:outline-none px-[10px] "
+                            placeholder="비밀번호"
                             type="password"
                             id="password"
                             value={password}
@@ -50,8 +50,12 @@ export default function LoginPage() {
                             required
                         />
                     </div>
-                    <button type="submit" className="w-[342px] h-[76px] bg-yellow-300 font-bold text-white text-lg">로그인</button>
+                    <button type="submit" className="w-[342px] h-[76px] text-[24px] bg-yellow-300 font-bold text-white text-lg">로그인</button>
                 </form>
+                <div className="relative bottom-[30px]">
+                    <span className="mr-[5px]">계정이 아직 없으신가요?</span>
+                    <span className="font-bold">회원가입</span>
+                </div>
             </section>
         </>
 
